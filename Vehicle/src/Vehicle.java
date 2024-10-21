1b0edda8a1fc13413f3bc2712920882adae4239e
Children313 <2755986965@qq.com>
2024-10-21 21:32:51 +0800

    initial commit of Vehicle Application

---
+++ Vehicle/src/Vehicle.java
@@ -0,0 +1,58 @@
+public class Vehicle {
+    private int passengers=5;
+    private String make="A Make";
+    private String model="ACME Model";
+    private float price=10000;
+    private int numWheels=5;
+
+
+    public Vehicle(int passengers, String make, String model, float price, int numWheels) {
+        this.passengers = passengers;
+        this.make = make;
+        this.model = model;
+        this.price = price;
+        this.numWheels = numWheels;
+    }
+
+    public int getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(int passengers) {
+        this.passengers = passengers;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getNumWheels() {
+        return numWheels;
+    }
+
+    public void setNumWheels(int numWheels) {
+        this.numWheels = numWheels;
+    }
+}
+
+
